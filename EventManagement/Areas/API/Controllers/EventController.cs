93c152013324bf056cef72bce2001c881d3b819a
biatahir <bia.tahir31@hotmail.com>
2017-11-22 17:45:34 -0800

    Bookmarked

---
+++ EventManagement/Areas/API/Controllers/EventController.cs
@@ -0,0 +1,27 @@
+﻿using EventManagement.Base;
+using EventManagement.BusinessLogic.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EventManagement.Areas.API.Controllers
+{
+    public class EventController : BaseAPIController
+    {
+        // GET: API/Event
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> ActivitiesByEvent(string Token, int eventId)
+        {
+            EventLogic logic = new EventLogic();
+            var data = await logic.ActivitiesByEvent(CurrentUserSession.AttendesID.Value, eventId);
+            return Json(new { Data = data });
+        }
+    }
+}
